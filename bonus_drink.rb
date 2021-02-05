417376a27ffdca50423c9a9a1dbc8a3648a4fced
KazuoY <p238074k@mail.kyutech.jp>
2021-02-05 21:25:32 +0900

    Initial commit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount < 3 then
+      return amount
+    elsif amount % 3 == 0 then
+      return amount + total_count_for(amount / 3)
+    else
+      return amount - amount % 3 + total_count_for(amount / 3 + amount % 3)
+    end
   end
 end
